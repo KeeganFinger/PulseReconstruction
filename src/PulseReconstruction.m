6bfc0bb5879a963f1ab8a563d40dc62c73671870
Keegan Finger <keegan.finger@colorado.edu>
2023-08-03 10:50:26 -0600

    Removed CPP source, chirp fit happens after unchirped fit

--- src/PulseReconstruction.m
+++ src/PulseReconstruction.m
@@ -7,7 +7,7 @@ known_harmonics = []; unknown_harmonics = [9];
 tau_max = 1000; dtau = 0.2;
 tmax = 500; tmin = -500;
 
-reconstruction_gaussian_list = [2] * size(unknown_harmonics,2);
+reconstruction_gaussian_list = [4] * size(unknown_harmonics,2);
 chirp = false; fit_color = false; cross_correlation = false;
 
 if cross_correlation
@@ -140,7 +140,7 @@ if cross_correlation
         N_free_states_l2,two_photon_dipoles_l2,l2_free_energies, ...
         N_bound_states_l1,two_photon_dipoles_l1,l1_bound_energies, ...
         N_free_states_l1,one_photon_dipoles_l1,l1_free_energies, ...
-        basis,known_laser.params(),delay,omega,chirp,N_gaussians,del_pos) ...
+        basis,known_laser.params(),delay,omega,false,N_gaussians,del_pos) ...
         ).^2,2),3));
 else
     calc = @(basis,delay,N_gaussians,del_pos) squeeze(sum(sum(abs( ...
@@ -149,7 +149,7 @@ else
         N_free_states_l2,two_photon_dipoles_l2,l2_free_energies, ...
         N_bound_states_l1,two_photon_dipoles_l1,l1_bound_energies, ...
         N_free_states_l1,one_photon_dipoles_l1,l1_free_energies, ...
-        basis,delay,omega,chirp,N_gaussians,del_pos) ...
+        basis,delay,omega,false,N_gaussians,del_pos) ...
         ).^2,2),3));
 end
 %%
@@ -170,6 +170,11 @@ else
 end
 
 %%
+%===== Spencer's weird parameters
+n_rep = 9;
+t_rep = 80;
+
+%%
 %===== Reconstruct Data =====================
 filter = @(time) interp1(correlation_delay,known,time);
 guesses = cell(size(reconstruction_gaussian_list));
@@ -180,9 +185,9 @@ for N_gaussians = reconstruction_gaussian_list
     for i = 1:N_gaussians
         initial_guess = [initial_guess; Laser(max_intensity / N_gaussians,0.5,500,1e-4,(-1).^i*floor(i/2)*gaussian_spacing,0);];
     end
-    guess = initial_guess.params(fit_color,chirp);
-    lower_bound = ones(N_gaussians,1) * Laser(1e-4 - 100i,0.2,1,-1,-max(correlation_delay)).params(fit_color,chirp);
-    upper_bound = ones(N_gaussians,1) * Laser(10 + 100i,1.5,1000,1,max(correlation_delay)).params(fit_color,chirp);
+    guess = initial_guess.params(fit_color,false);
+    lower_bound = ones(N_gaussians,1) * Laser(1e-4 - 100i,0.2,1,-1,tmin).params(fit_color,false);
+    upper_bound = ones(N_gaussians,1) * Laser(10 + 100i,1.5,1000,1,tmax).params(fit_color,false);
     if fit_color
         del_pos = [4 3] * size(guess,1) - size(guess,1) + 1;
     else
@@ -199,14 +204,26 @@ for N_gaussians = reconstruction_gaussian_list
     
     for percent = percentages
         disp(percent)
-        sample_tau = 0:sample_step:percent*window_width/100;
-        tau = sample_tau;
-        for window = 1:N_windows-1
-            tau = [tau sample_tau + window*window_width];
+        tau0 = 0:sample_step:percent/max_frequency;
+        tau = tau0;
+        if percent / max_frequency < t_rep
+            if n_rep > 1
+                for ir = 2:n_rep
+                    tau = [tau, tau0+(ir-1)*t_rep];
+                end
+            end
+        else
+            tau = correlation_delay;
         end
-        sample_data = filter(tau)';
+        sample_data = filter(tau);
+
+%         tau = sample_tau;
+%         for window = 1:N_windows-1
+%             tau = [tau sample_tau + window*window_width];
+%         end
+%         sample_data = filter(tau)';
     
-        err = @(basis) abs(calc(basis,tau,N_gaussians,del_pos) - sample_data)./max(abs(sample_data));
+        err = @(basis) abs(calc(basis,tau,N_gaussians,del_pos)' - sample_data)./max(abs(sample_data));
     
         new_guess = lsqnonlin(err,guess,lower_bound,upper_bound,options);
         guess = new_guess;
@@ -215,6 +232,49 @@ for N_gaussians = reconstruction_gaussian_list
         guess = [guess(1:pos-1) 0 guess(pos:end)];
     end
     guess = reshape(guess,N_gaussians,[]);
+    if chirp
+        if cross_correlation
+            calc = @(basis,delay,N_gaussians,del_pos) squeeze(sum(sum(abs( ...
+                matrixElementsCalculation_xcorr(initial_energy, ...
+                N_free_states_l0,two_photon_dipoles_l0,l0_free_energies, ...
+                N_free_states_l2,two_photon_dipoles_l2,l2_free_energies, ...
+                N_bound_states_l1,two_photon_dipoles_l1,l1_bound_energies, ...
+                N_free_states_l1,one_photon_dipoles_l1,l1_free_energies, ...
+                basis,known_laser.params(),delay,omega,true,N_gaussians,del_pos) ...
+                ).^2,2),3));
+        else
+            calc = @(basis,delay,N_gaussians,del_pos) squeeze(sum(sum(abs( ...
+                matrixElementsCalculation(initial_energy, ...
+                N_free_states_l0,two_photon_dipoles_l0,l0_free_energies, ...
+                N_free_states_l2,two_photon_dipoles_l2,l2_free_energies, ...
+                N_bound_states_l1,two_photon_dipoles_l1,l1_bound_energies, ...
+                N_free_states_l1,one_photon_dipoles_l1,l1_free_energies, ...
+                basis,delay,omega,true,N_gaussians,del_pos) ...
+                ).^2,2),3));
+        end
+        fit_chirp = @(chirp_params) abs(calc([guess chirp_params],correlation_delay,N_gaussians,[]) - known)./max(abs(known));
+        chirp_params = lsqnonlin(fit_chirp,zeros(N_gaussians,1),[],[],options);
+        guess = [guess chirp_params];
+        if cross_correlation
+            calc = @(basis,delay,N_gaussians,del_pos) squeeze(sum(sum(abs( ...
+                matrixElementsCalculation_xcorr(initial_energy, ...
+                N_free_states_l0,two_photon_dipoles_l0,l0_free_energies, ...
+                N_free_states_l2,two_photon_dipoles_l2,l2_free_energies, ...
+                N_bound_states_l1,two_photon_dipoles_l1,l1_bound_energies, ...
+                N_free_states_l1,one_photon_dipoles_l1,l1_free_energies, ...
+                basis,known_laser.params(),delay,omega,false,N_gaussians,del_pos) ...
+                ).^2,2),3));
+        else
+            calc = @(basis,delay,N_gaussians,del_pos) squeeze(sum(sum(abs( ...
+                matrixElementsCalculation(initial_energy, ...
+                N_free_states_l0,two_photon_dipoles_l0,l0_free_energies, ...
+                N_free_states_l2,two_photon_dipoles_l2,l2_free_energies, ...
+                N_bound_states_l1,two_photon_dipoles_l1,l1_bound_energies, ...
+                N_free_states_l1,one_photon_dipoles_l1,l1_free_energies, ...
+                basis,delay,omega,false,N_gaussians,del_pos) ...
+                ).^2,2),3));
+        end
+    end
     err = @(basis) abs(calc(basis,correlation_delay,N_gaussians,[]) - known)./max(abs(known));
     ion_error{ind} = sqrt(dtau * sum(err(guess).^2));
     guesses{ind} = Laser.generate(guess,chirp,omega*ones(N_gaussians,1));
